7b72128d26bff682784b390051d63c14b2c527c2
Gustavo_Martins <gtavomartins893@gmail.com>
2022-08-09 23:32:45 -0300

    translated progs 11 and 12 from List 04

--- list04-RepeatingLoops/prog11.c
+++ list04-RepeatingLoops/prog11.c
@@ -3,16 +3,16 @@
 int main() {
     int n;
     int res = 0;
-    int impar = 1;
-    printf("Informe um valor natural: ");
+    int odd = 1;
+    printf("Enter a natural value: ");
     scanf(" %d", &n);
 
     for(int i = 0; i < n; i++) {
-        res += impar;
-        impar += 2;
+        res += odd;
+        odd += 2;
     }
 
-    printf("\nO quadrado de %d eh: %d", n, res);
+    printf("\n%d squared is: %d", n, res);
 
     return 0;
 }
--- list04-RepeatingLoops/prog12.c
+++ list04-RepeatingLoops/prog12.c
@@ -2,19 +2,19 @@
 
 int main() {
     int n;
-    int primo = 1; // true / false
-    printf("Numero: ");
+    int prime = 1; // true / false
+    printf("Number: ");
     scanf("%d", &n);
 
     for(int i = 2; i < n; i++) {
         if(n % i == 0) {
-            primo = 0;
+            prime = 0;
         }
     }
-    if(primo == 1) {
-        printf("Primo\n");
+    if(prime == 1) {
+        printf("Prime\n");
     } else {
-        printf("Nao Primo\n");
+        printf("Not Prime\n");
     }
 
     return 0;
